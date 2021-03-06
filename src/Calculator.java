56d4a988dbfc3df222331daba3fd30087283fc37
lparadylo <lukaszparadylo@gmail.com>
2021-03-06 11:50:20 +0100

    Add methods to Calculator class

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,16 @@
 public class Calculator {
+
+    private Double sum;
+
+    public Double addTwoNumbers(Double x, Double y){
+        this.sum = x+y;
+        return this.sum;
+    }
+    public Double substractTwoNumbers(Double x, Double y){
+        this.sum = x-y;
+        return this.sum;
+    }
+    public Double getSum() {
+        return this.sum;
+    }
 }
